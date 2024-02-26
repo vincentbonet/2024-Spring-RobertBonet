3113982ce345befcc33279ea17a7d386f731b680
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-02-26 14:13:37 -0500

    changes

---
+++ vue-project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./midtermproject.html",
+    "./midtermproject.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
