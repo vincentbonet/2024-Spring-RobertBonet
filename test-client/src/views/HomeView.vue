0469871d253f247c7274cb539d81774453785370
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-02-26 14:36:56 -0500

    bunch of chnages

---
+++ test-client/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'HomeView',
+});
+</script>
+
+<template>
+  <main class="bg-gray-100">
+    <!-- Your home page content here -->
+    
+ 
+  </main>
+</template>
---
+++ test-client/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["C:/Users/robon/OneDrive/Documents/GitHub/2024-Spring-RobertBonet/test-client/src/assets/main.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
