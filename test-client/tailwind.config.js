848aef08a4b79c82962777b350a443e361838056
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-02-29 15:14:46 -0500

    more and more changes

--- test-client/tailwind.config.js
+++ test-client/tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './src/**/*.ts',
-    './src/**/*.tsx',
-  ],
+  purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  content: [],
   theme: {
     extend: {},
   },
