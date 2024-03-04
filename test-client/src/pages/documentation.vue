882ad228b66c8c1cc75f600aae3bdcc0772676f0
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-03-04 14:12:07 -0500

    commited changes

---
+++ test-client/src/pages/documentation.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-4xl">This is the documentation page</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
